ed514edfc694799336f320688fa814e7b4857559
Jamie Wright <jamie@brilliantfantastic.com>
2013-01-05 13:07:26 -0500

    1-CreateProject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubFantasyLeague::Application.config.secret_token = '288dcbf8eb41c4f289989af21601d67622614e1a65b3df80a44bea1ef60e341828f86e577b589be52e6c7e53181685787bb40aeb9ad2ae4d8cf90c8bdcb2a2e3'
